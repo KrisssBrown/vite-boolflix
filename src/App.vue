fc3d6edbb325b4dedad1dd9b1919f51aaf72c50e
KrisssBrown <cristianbruno.cs@gmail.com>
2023-03-08 09:35:21 +0100

    START

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <Header />
+    <Main />
+
+    
+
+    <main>
+
+    </main>
+  </div>
+</template>
+
+<!-- ****************************************************************************** -->
+
+<script>
+import Header from './components/Header.vue';
+import axios from 'axios'
+
+import Main from './components/Main.vue';
+
+  export default {
+    components:{
+      Header,
+      Main
+    },
+
+    data(){
+      return{
+        filmCards: [],
+      }
+    },
+
+    methods:{
+      fetchFilm(){
+        axios.get(` https://api.themoviedb.org/3/search/movie?api_key=73da7b7926084309d0c522cac935636c&query` , {
+        params: {
+          
+        }
+      }).then((res) => {
+        // this.filmCards = res.data.data
+        console.log(res)
+      })
+      }
+    },
+
+    created(){
+
+    }
+    
+  }
+</script>
+
+<!-- ****************************************************************************** -->
+
+<style lang="scss">
+@use './style/general.scss' as *;
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+    <header class="container flex">
+        <h1>
+            <a href="#">
+                Boolflix
+            </a>
+        </h1>
+
+        <input type="text" placeholder="Cerca un film...">
+    </header>
+</template>
+
+<!-- ****************************************************************************** -->
+
+
+<script>
+export default {
+
+}
+</script>
+
+
+<!-- ****************************************************************************** -->
+<style lang="scss" scoped>
+
+// .container{
+//     background-color: black;
+// }
+
+    h1{
+        color: red;
+    }
+.flex{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 15px 0;
+}
+
+</style>
