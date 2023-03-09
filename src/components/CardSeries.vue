9a8c8990e1839fdb913bbc310ff71fa6049009d7
KrisssBrown <cristianbruno.cs@gmail.com>
2023-03-09 23:04:30 +0100

    REFACTORING

--- src/components/CardSeries.vue
+++ src/components/CardSeries.vue
@@ -46,12 +46,5 @@ li {
     color: white;
 }
 
-<<<<<<< HEAD
-=======
-// .grid {
-//     display: grid;
-//     grid-template-columns: repeat(5, 1fr);
-//     gap: 10px;
-// }
->>>>>>> 3547979c9e160095052b0ad935eeec59e3d65a4e
+
 </style>
